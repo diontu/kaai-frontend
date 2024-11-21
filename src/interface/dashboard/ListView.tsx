2346466a86e8b567d54e75c8ad0affb185ebc9f0
diontugit <diontugit@gmail.com>
2024-11-21 00:13:01 -0500

    minor: add list view table

--- src/interface/dashboard/ListView.tsx
+++ src/interface/dashboard/ListView.tsx
@@ -1,5 +1,42 @@
-const ListView = (): JSX.Element => {
-  return <div>ListView</div>;
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+type ListViewProps = {
+  data: {
+    id: number;
+    title: string;
+    description: string;
+    image: string;
+  }[];
+};
+
+const ListView = ({ data }: ListViewProps): JSX.Element => {
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Recipe Name</TableHead>
+          <TableHead>Protein</TableHead>
+          <TableHead>Carbs</TableHead>
+          <TableHead>Fat</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        <TableRow>
+          <TableCell>Linguine</TableCell>
+          <TableCell>56g</TableCell>
+          <TableCell>12g</TableCell>
+          <TableCell>8g</TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  );
 };
 
 export default ListView;
