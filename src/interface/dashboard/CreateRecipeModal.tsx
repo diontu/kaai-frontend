a8510cf9a4c24731d793a77cdb0405989d1c53fa
diontugit <diontugit@gmail.com>
2024-12-04 20:39:11 -0500

    minor: add more fields to modal

--- src/interface/dashboard/CreateRecipeModal.tsx
+++ src/interface/dashboard/CreateRecipeModal.tsx
@@ -9,24 +9,81 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectTrigger,
+  SelectValue,
+  SelectItem,
+  SelectContent,
+} from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
 import { PropsWithChildren } from "react";
 
-const CreateRecipeModal = ({ children }: PropsWithChildren) => {
+type Difficulty = "easy" | "medium" | "hard";
+
+const difficultyOptions: { value: Difficulty; label: string }[] = [
+  {
+    value: "easy",
+    label: "Easy",
+  },
+  {
+    value: "medium",
+    label: "Medium",
+  },
+  {
+    value: "hard",
+    label: "Hard",
+  },
+];
+
+const CreateRecipeModalHeader = (): JSX.Element => {
+  return (
+    <DialogHeader>
+      <DialogTitle className="text-2xl">Add New Recipe</DialogTitle>
+    </DialogHeader>
+  );
+};
+
+const CreateRecipeModal = ({ children }: PropsWithChildren): JSX.Element => {
   return (
     <div>
       <Dialog>
         <DialogTrigger asChild>{children}</DialogTrigger>
         <DialogContent>
-          <DialogHeader>
-            <DialogTitle className="text-2xl">Add New Recipe</DialogTitle>
-            <DialogDescription>
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
-            </DialogDescription>
-          </DialogHeader>
-
-          <Input placeholder="Enter recipe name" />
-          <Input placeholder="Enter cooking instructions" />
+          <CreateRecipeModalHeader />
+          <div>
+            <Label htmlFor="recipeName">Recipe Name</Label>
+            <Input id="recipeName" placeholder="Enter recipe name" />
+          </div>
+          <div>
+            <Label htmlFor="instructions">Instructions</Label>
+            <Textarea
+              id="instructions"
+              placeholder="Enter recipe description"
+            />
+          </div>
+          <div className="flex gap-2">
+            <div className="w-full">
+              <Label htmlFor="cookingTime">Cooking Time (minutes)</Label>
+              <Input id="cookingTime" placeholder="e.g. 30" />
+            </div>
+            <div className="w-full">
+              <Label htmlFor="difficulty">Difficulty</Label>
+              <Select>
+                <SelectTrigger id="difficulty">
+                  <SelectValue placeholder="Select difficulty" />
+                </SelectTrigger>
+                <SelectContent>
+                  {difficultyOptions.map((difficulty) => (
+                    <SelectItem value={difficulty.value}>
+                      {difficulty.label}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            </div>
+          </div>
           <DialogFooter className="mt-6">
             <Button className="w-full" variant={"outline"}>
               Save Draft
